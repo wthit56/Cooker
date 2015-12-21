f102da23efe055c8a9f51e32a4545acc4bd29ae7
Thomas Giles <wthit56@googlemail.com>
2015-12-21 12:21:42 +0000

    Moved old tests

--- script.js
+++ script.js
@@ -146,10 +146,3 @@ function next(inst) {
 	var nextInst = inst.nextElementSibling;
 	alert("Cooking needs your attention!");
 }
-
-/*
-add("Pizza", 12, 160);
-add("Chips", 10, 200);
-add("Other Pizza", 9.95, 200);
-render(recalc());
-*/
